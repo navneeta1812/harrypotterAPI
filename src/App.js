81da4b803dce8c642516af319d9b0a84e9bb93aa
Navneeta <navneetaprasad1812@gmail.com>
2021-06-24 02:43:42 +0530

    api integration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [characterList, setcharacterList] = useState([])
+  useEffect(() => {
+    fetch("http://hp-api.herokuapp.com/api/characters") 
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      alert(err)
+    })
+   
+  }, [])
   return (
     <div className="App">
-    <h1 >Harry Potter</h1>
+      
+
+    
     </div>
   );
-  
+  //card details
+  /*name, house, dateOfBirth, ancestry, patronous, actor, alive, image */
 }
 
 export default App;
